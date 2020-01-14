152f683ad19bace2b53e24d4d82607a90e401cd7
github username <github email address>
2020-01-14 10:26:46 -0500

    frontend added

---
+++ src/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const freePhoneList = document.querySelector("#free_phone_list");
+    let freePhones = [];
+    const freePhoneVideo = document.querySelector("#free_phone_video");
+
+    fetch("http://localhost:3000/free_phones")
+    .then(response => response.json())
+    .then(freePhonesArray => {
+      console.log(freePhonesArray);
+      freePhones = freePhonesArray;
+      freePhones.forEach(freePhone => {
+        const freePhoneBullet = document.createElement("li");
+        freePhoneBullet.id = freePhone.id;
+        freePhoneBullet.innerText = freePhone.description;
+        freePhoneList.appendChild(freePhoneBullet);
+      })
+    });
+
+    document.addEventListener("click", (event) => {
+        // debugger
+        // console.log(event.target);
+        if (event.target.tagName === "LI") {
+            console.log("lolnope nice try")
+            // debugger
+            // console.log(event.target.innerText);
+            const foundFreePhone = freePhones.find(freePhone => freePhone.id === parseInt(event.target.id))
+            let lastListItem = freePhoneList.lastElementChild;
+            while (lastListItem) {
+              freePhoneList.removeChild(lastListItem);
+              lastListItem = freePhoneList.lastElementChild;
+            }
+            freePhoneVideo.innerHTML = `<iframe width="560" height="315" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            const videoCode = foundFreePhone.url.slice(32);
+            document.querySelector("iframe").src = "https://www.youtube.com/embed/" + videoCode + "?autoplay=1"
+            // debugger
+        }
+    });
+});
